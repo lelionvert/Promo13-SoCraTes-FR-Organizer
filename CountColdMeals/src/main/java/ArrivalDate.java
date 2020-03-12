b183e18ff8c2b96a1866634a64a7476b608d95ff
abderrezak <abderrezak.khif@gmail.com>
2020-03-12 15:11:16 +0100

    refacto cases for arrivalDate

--- CountColdMeals/src/main/java/ArrivalDate.java
+++ CountColdMeals/src/main/java/ArrivalDate.java
@@ -1,7 +1,6 @@
 public class ArrivalDate {
 
 
-
     public static final Integer END_OF_DAY = 24;
     public static final Integer MIDNIGHT = 0;
     private static final Integer FIRST_DAY = 4;
@@ -32,7 +31,7 @@ public class ArrivalDate {
     }
 
     public static ArrivalDate createDateHourMinuteDay(Integer hour, Integer minute, Integer day) {
-        return new ArrivalDate(hour,minute,day);
+        return new ArrivalDate(hour, minute, day);
     }
 
     public Integer getHour() {
@@ -48,17 +47,15 @@ public class ArrivalDate {
     }
 
     boolean isInTheRightTime() {
-        if(MIDNIGHT.equals(hour) && FIRST_MINUTE.equals(minute) && day.equals(FIRST_DAY + 1 )){
-            return true;
-        }
-        if (! isOnTheFirstDay()) {
-            return false;
-        }
-        return isAfterLimitTime();
+        return isArrivedAtLimit() || (isOnTheFirstDay() && isAfterStartTime());
     }
 
-    private boolean isAfterLimitTime() {
-        if (END_OF_HOT_MEALS.equals(hour)){
+    private boolean isArrivedAtLimit() {
+        return MIDNIGHT.equals(hour) && FIRST_MINUTE.equals(minute) && FIRST_DAY.equals(day - 1);
+    }
+
+    private boolean isAfterStartTime() {
+        if (END_OF_HOT_MEALS.equals(hour)) {
             return minute > 0;
         } else {
             return hour > END_OF_HOT_MEALS && hour < END_OF_DAY;
