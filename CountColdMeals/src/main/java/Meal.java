0692ef1af2feae711372ead463a15fbab14e12e4
abderrezak <abderrezak.khif@gmail.com>
2020-03-12 12:19:30 +0100

    Remonving useless fields and constructors

--- CountColdMeals/src/main/java/Meal.java
+++ CountColdMeals/src/main/java/Meal.java
@@ -1,25 +1,10 @@
 public class Meal {
 
-    private int participants;
-    private int punctualParticipants;
-
-
     private Participant participant;
-
-    public Meal(int participants) {
-        this.participants = participants;
-        this.punctualParticipants = 0;
-    }
-
-    public Meal(int participants, int punctualParticipants) {
-        this.participants = participants;
-        this.punctualParticipants = punctualParticipants;
-    }
 
     public Meal(Participant participant) {
         this.participant = participant;
     }
-
 
     public int getColdMeals() {
         if (! this.participant.haveAColdMeal()){
