d30d9d1e9295923eb54063d8269f85dd36489235
abderrezak <abderrezak.khif@gmail.com>
2020-03-13 09:59:11 +0100

    refacto calculate price

--- PriceCalculator/src/main/java/lacombe/BlackBox.java
+++ PriceCalculator/src/main/java/lacombe/BlackBox.java
@@ -6,30 +6,47 @@ public class BlackBox {
     public static final String NO_ACCOMMODATION = "No Accommodation";
     public static final String TWIN = "Twin";
     public static final String TRIPLE = "Triple";
+    public static final int MEAL_PRICE = 40;
 
     public static long calculatePrice(String input) {
-        if (input.contains(SINGLE)) {
+        if (isSingle(input)) {
             if(input.contains(("vendredi"))){
-                return 570;
+                return 610 - MEAL_PRICE;
             }
             return 610;
-        } else if (input.contains(NO_ACCOMMODATION)) {
+        } else if (isNoAccomodation(input)) {
             if (input.contains("samedi")) {
-                return 200;
+                return 240 - MEAL_PRICE;
             }
             return 240;
-        } else if (input.contains(TWIN)){
+        } else if (isTwin(input)){
             if (input.contains(("samedi"))) {
-                return 470;
+                return 510 - MEAL_PRICE;
             }
             return 510;
-        } else if (input.contains(TRIPLE)) {
+        } else if (isTriple(input)) {
             if(input.contains("vendredi")){
-                return 370;
+                return 410 - MEAL_PRICE;
             }
             return 410;
         } else {
             return 0;
         }
+    }
+
+    private static boolean isTriple(String input) {
+        return input.contains(TRIPLE);
+    }
+
+    private static boolean isTwin(String input) {
+        return input.contains(TWIN);
+    }
+
+    private static boolean isNoAccomodation(String input) {
+        return input.contains(NO_ACCOMMODATION);
+    }
+
+    private static boolean isSingle(String input) {
+        return input.contains(SINGLE);
     }
 }
