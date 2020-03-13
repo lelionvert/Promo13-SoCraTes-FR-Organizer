d7128f5a75e59e0b0253ab7ebba685542b7ded3b
abderrezak <abderrezak.khif@gmail.com>
2020-03-13 16:38:21 +0100

    refactor calculatePrice

--- PriceCalculator/src/main/java/lacombe/PriceCalculator.java
+++ PriceCalculator/src/main/java/lacombe/PriceCalculator.java
@@ -1,28 +1,12 @@
 package lacombe;
 
-import static lacombe.Choice.*;
-
 public class PriceCalculator {
 
     public static final int MEAL_PRICE = 40;
 
     public static long calculatePrice(String input) {
-        CheckIn checkIn = new CheckIn(input);
-        Choice choice = checkIn.getChoice();
-        if (CheckIn.isSingle(input)) {
-            return calculatePriceFor(input, SINGLE);
-        } else if (CheckIn.isNoAccomodation(input)) {
-            return calculatePriceFor(input, NO_ACCOMMODATION);
-        } else if (CheckIn.isTwin(input)) {
-            return calculatePriceFor(input, TWIN);
-        } else if (CheckIn.isTriple(input)) {
-            return calculatePriceFor(input, TRIPLE);
-        } else {
-            return 0;
-        }
-    }
-
-    private static long calculatePriceFor(String input, Choice choice) {
+        Parser parser = new Parser(input);
+        Choice choice = parser.getChoice();
         if (CheckIn.isArrivedOnFriday(input) || CheckIn.isDepartureOnSaturday(input)) {
             return choice.getPrice() - MEAL_PRICE;
         }
